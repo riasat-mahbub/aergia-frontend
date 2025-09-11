4724ea518278a99d9e8b1fb65203a3254e39b20d
riasat1998 <riasat1998@gmail.com>
2025-09-11 00:18:23 -0300

    login is now needed to create resume

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,13 +3,34 @@ import { useApi } from "@/hooks/useApi";
 import { apiService } from "@/services/api";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
 
   const { execute, loading, error } = useApi();
   const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleBuilder = () => {
+    if(isLoggedIn){
+      router.push('/builder')
+    }else{
+      router.push('/login')
+    }
+  }
+
+    useEffect(() => {
+    const checkIfLoggedIn = async () => {
+      const result = await execute(() => apiService.auth.isLoggedIn());
+      
+      if (result) {
+       setIsLoggedIn(true)
+      }
+    }
+
+    checkIfLoggedIn();
+  }, [])
 
 
   return (
@@ -26,9 +47,9 @@ export default function Home() {
         <p className="h-20">
 
         </p>
-        <Link href="/importer" className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-full">
+        <div onClick={handleBuilder} className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-full w-44 cursor-pointer">
           Create Resume <span aria-hidden="true">→</span>
-        </Link>
+        </div>
         {/* <p className="mt-3 text-sm text-gray-600 lg:mt-36">
           Already have a resume? Test its ATS readability with the{" "}
           <Link href="/resume-parser" className="underline underline-offset-2">
