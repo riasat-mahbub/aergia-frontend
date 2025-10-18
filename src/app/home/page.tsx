a423abdde6c2ffb2d538cd368c862971bc36b9f5
riasat1998 <riasat1998@gmail.com>
2025-10-18 02:42:04 -0300

    added some flair to homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,8 @@ import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { RootState } from "@/store/store";
 import { useEffect, useState } from "react";
-
+import * as motion from "motion/react-client"
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function Home() {
   const { isLoggedIn } = useSelector((state: RootState) => state.auth);
@@ -28,21 +29,25 @@ export default function Home() {
 
   return (
     <section className="lg:flex  lg:justify-center lg:items-center">
-      <div className="mx-auto max-w-xl pt-8 text-center lg:mx-0 lg:grow lg:pt-32 lg:text-left">
-        <h1 className="text-primary pb-2 text-4xl font-bold lg:text-5xl">
+      <div className="mx-auto pt-8 text-center lg:pt-32 lg:text-left flex flex-col">
+        <h1 className="text-primary pb-2 font-bold text-4xl md:text-7xl mt-44">
           Create a professional
           <br />
-          resume easily
+          resume in minutes
         </h1>
-        <p className="mt-3 text-lg lg:mt-5 lg:text-xl">
-          With this free and powerful resume builder
+        <p className="mt-3 text-lg lg:mt-5 md:text-3xl mx-3 lg:mx-0">
+         With <span className="text-emerald-700 font-bold">Aergia</span>, the free and powerful resume builder
         </p>
-        <p className="h-20">
-
+        <p className="mt-3 textmd lg:mt-5 md:text-xl mb-10 text-gray-700">
+          Click the button below to get started 
         </p>
-        <div onClick={handleBuilder} className={`bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-full w-44 cursor-pointer ${!isInitialized ? 'opacity-50' : ''}`}>
-          Create Resume <span aria-hidden="true">→</span>
-        </div>
+        <motion.div  whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
+          onClick={handleBuilder} 
+          className={`self-center lg:self-auto bg-emerald-500 hover:bg-emerald-700 text-white
+            font-bold p-4 rounded-full w-44 lg:w-52 lg:text-xl cursor-pointer flex flex-row ${!isInitialized ? 'opacity-50' : ''}`}>
+          
+          Create Resume <span aria-hidden="true"> <ChevronRight/> </span>
+        </motion.div>
         {/* <p className="mt-3 text-sm text-gray-600 lg:mt-36">
           Already have a resume? Test its ATS readability with the{" "}
           <Link href="/resume-parser" className="underline underline-offset-2">
